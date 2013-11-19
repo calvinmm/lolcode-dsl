3dde700fe7aaded6e1b6e1efcb0fd535d51e1505
Calvin MacKenzie <calvinm.mackenzie@gmail.com>
2013-11-19 17:21:13 -0600

    forgot to save KTHXBYE

--- src/main/scala/lolcode/LolCode.scala
+++ src/main/scala/lolcode/LolCode.scala
@@ -1,76 +1,71 @@
 package lolcode
 import scala.collection.mutable._
 
-class LolCode 
-{
-    abstract sealed class LolLine
-    case class PrintString(num: Int, s: String) extends LolLine 
-    case class End(num: Int) extends LolLine
+class LolCode {
+  abstract sealed class LolLine
+  case class PrintString(num: Int, s: String) extends LolLine
+  case class End(num: Int) extends LolLine
 
+  var lines = new HashMap[Int, LolLine]
+  var current = 1
 
-    var lines = new HashMap[Int, LolLine]
-    var current = 1
-
-    def KTHXBYE() = 
-    { 
+  def KTHXBYE() =
+    {
       lines(current) = End(current)
       gotoLine(lines.keys.toList.sorted.head)
     }
 
-    def HAI() = { var lines = new HashMap[Int, LolLine] }
-    /**
-     * LineBuilder is the jump off point for the line number syntax of
-     * BASIC expressions.  Simply put, the `int2LineBuilder` implict
-     * puts an instance of this class in place when it finds an Int
-     * followed by one of the provided methods.  That is, this *only* handles
-     * the forms that follow a line number and no other forms.  If you need to
-     * add new functions that can be put elsewhere, then follow the form
-     * used by SQRT and ABS.  For functions that can go *anywhere*, they should
-     * be handled here and as separate functions as SQRT.
-     */
-    case class LineBuilder(num: Int) {
-      def END() = lines(num) = End(num)
+  def HAI() = { var lines = new HashMap[Int, LolLine] }
+  /**
+   * LineBuilder is the jump off point for the line number syntax of
+   * BASIC expressions.  Simply put, the `int2LineBuilder` implict
+   * puts an instance of this class in place when it finds an Int
+   * followed by one of the provided methods.  That is, this *only* handles
+   * the forms that follow a line number and no other forms.  If you need to
+   * add new functions that can be put elsewhere, then follow the form
+   * used by SQRT and ABS.  For functions that can go *anywhere*, they should
+   * be handled here and as separate functions as SQRT.
+   */
+  case class LineBuilder(num: Int) {
+    def END() = lines(num) = End(num)
 
-      object PRINT {
-        def apply(str:String) = lines(num) = PrintString(num, str)
+    object PRINT {
+      def apply(str: String) = lines(num) = PrintString(num, str)
     }
   }
 
-
-    /**
-     * This is the runtime evaluator of the built Scala classes from the
-     * original BASIC forms.  Currently, lines can only be incremented by
-     * 10, otherwise you program might not act the way you expect.
-     */
-    private def gotoLine(line: Int) {
-      lines(line) match {
-        case PrintString(_, s:String) => {
-          println(s)
-          gotoLine(line + 1)
-        }
-        case End(_) => {
-          println()
-        }
-        case _ => {
-          println()
-        }
+  /**
+   * This is the runtime evaluator of the built Scala classes from the
+   * original BASIC forms.  Currently, lines can only be incremented by
+   * 10, otherwise you program might not act the way you expect.
+   */
+  private def gotoLine(line: Int) {
+    lines(line) match {
+      case PrintString(_, s: String) => {
+        println(s)
+        gotoLine(line + 1)
+      }
+      case End(_) => {
+        println()
+      }
+      case _ => {
+        println()
       }
     }
+  }
 
-    implicit def newline(i: Int) = LineBuilder(i)
-    
-    object VISIBLE
-    {
-      def apply(s: String) = 
+  implicit def newline(i: Int) = LineBuilder(i)
+
+  object VISIBLE {
+    def apply(s: String) =
       {
         lines(current) = PrintString(current, s)
         current += 1
       }
-    }
+  }
 
-    object Lol
-    {
-      def apply(s: String) = println(s);
-      def VISIBLE(s: String) = apply(s)
-    }
+  object Lol {
+    def apply(s: String) = println(s);
+    def VISIBLE(s: String) = apply(s)
+  }
 }
--- src/main/scala/lolcode/LolTest.scala
+++ src/main/scala/lolcode/LolTest.scala
@@ -1,14 +1,13 @@
 package lolcode
 
-object LolTest extends LolCode
-{
-    def main(args: Array[String]): Unit = 
+object LolTest extends LolCode {
+  def main(args: Array[String]): Unit =
     {
-        HAI
-        VISIBLE("hello world")
+      HAI
+      VISIBLE("hello world")
 
-        Lol VISIBLE "hello world 2"
+      Lol VISIBLE "hello world 2"
 
-        KTHXBYE
+      KTHXBYE
     }
 }
