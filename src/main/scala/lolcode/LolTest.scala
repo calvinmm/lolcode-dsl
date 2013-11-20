a2062fc2cd394dab94120b44c587ab1df2e5fc0c
Corbyn Salisbury <corbynsal@utexas.edu>
2013-11-20 08:16:15 -0600

    Added start of Test2, split Test1 out

--- src/main/scala/lolcode/LolTest.scala
+++ src/main/scala/lolcode/LolTest.scala
@@ -4,12 +4,28 @@ object LolTest extends LolCode
 {
   def main(args: Array[String]): Unit = 
   {
+
+    Test1()
+    println("TEST 1 PASSED!\n\n")
+
+    Test2()
+  }
+
+  def Test1(): Unit =
+  {
     HAI
     VISIBLE ("hello world")
     I_HAZ_A ('a) ITZ 1
     VISIBLE ('a)
     Lol VISIBLE "hello world 2"
 
+    KTHXBYE
+  }
+
+  def Test2(): Unit =
+  {
+    HAI
+    VISIBLE ("Test 2 IN THE FLESH")
     KTHXBYE
   }
 }
